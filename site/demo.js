ff3b78de2adeec34b9f18ca10cef1c3f8818f845
T.J. Purtell <tpurtell@stanford.edu>
2012-03-29 02:27:32 -0700

    add bare site. move around files.

---
+++ site/demo.js
@@ -0,0 +1,42 @@
+$(document).ready(init);
+var canvas, filterValues, filters;
+var easeSelected = false;
+var defaultEase = "out";
+var defaultTime = 1000;
+var defaultSteps = 15;
+function init(){
+	filters = $.fn.jsManipulate("getFilters");
+	$(":checkbox").checkbox();
+	var timeSlider = $("#time-slider").slider({
+		animate: "fast",
+		min: 0,
+		max: 5000,
+		step: 100,
+		value: defaultTime,
+		change: function() {
+			$("#time-input").attr({value: $(this).slider("option","value") });	
+		}
+	});
+	var stepsSlider = $("#steps-slider").slider({
+		animate: "fast",
+		min: 0,
+		max: 100,
+		value: defaultSteps,
+		step: 1,
+		change: function() {
+			$("#steps-input").attr({value: $(this).slider("option","value") });	
+		}
+	});
+	$("#time-input").attr({value: defaultTime}).change(function(){timeSlider.slider({value:$(this).attr("value")});});
+	$("#steps-input").attr({value: defaultSteps}).change(function(){stepsSlider.slider({value:$(this).attr("value")});});
+	$("#image").jsManipulate("convert", function(can){ canvas=can;});
+	fillFilterList();  
+	$("#filter-select")[0].value = "twirl";
+	loadFilterValues(); 
+	$("#direction-select, #filter-select").selectmenu();
+	$("#direction-select").selectmenu("disable");
+	$("#easing-select").selectmenu({change: function(){ easeSelected = true; }});
+	$("#apply-button").click(function(){ applyFilter("apply");});
+	$("#animate-button").click(function(){ applyFilter("animate");});
+
+}
